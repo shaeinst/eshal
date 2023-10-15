cd3298f9dadbe10bbab8ef4b0d0eddbe25d1960e
Ali Shahid <shaeinst@gmail.com>
2023-10-15 11:58:58 +0545

    sss

--- src/exporters/component.ts
+++ src/exporters/component.ts
@@ -1,6 +1,5 @@
 // BUTTONS
 export { default as PrimaryButton } from '../interfaces/components/Buttons/Primary/PrimaryButton'
-export { default as HeadlessButton } from '../interfaces/components/Buttons/HeadlessButton'
 
 // FORMS
 export { default as CredInput } from '../interfaces/components/Forms/CredInput/CredInput'
