7d33176dda1e09e85958db2ac3c371636fb7c57a
Ali Shahid <shaeinst@gmail.com>
2024-07-07 12:37:45 +0400

    feat: flashlist now support new arch

--- react-native.config.js
+++ react-native.config.js
@@ -3,12 +3,6 @@ module.exports = {
         android: {
             unstable_reactLegacyComponentNames: [
                 /**
-                 * will remove this once flash-list support new architecture
-                 * https://github.com/Shopify/flash-list/issues/811#issuecomment-1624778573
-                 */
-                'CellContainer',
-                'AutoLayoutView',
-                /**
                  * will remove this once react-native-fast-image support new architecture
                  * https://github.com/DylanVann/react-native-fast-image
                  */
